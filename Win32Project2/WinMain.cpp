d4ce42694a7f289f9ef28b736c85576901679bb5
Subin Park <dlakek@naver.com>
2015-03-18 15:56:44 +0900

    두번째 과제 준비

---
+++ Win32Project2/WinMain.cpp
@@ -0,0 +1,93 @@
+#include <windows.h>
+#include <time.h>
+
+#define szWindowClass	TEXT("First")
+#define szTitle			TEXT("First App")
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+int APIENTRY WinMain( HINSTANCE hInstance,
+					  HINSTANCE hPrevInstance,
+					  LPSTR lpszCmdParam,
+					  int nCmdShow)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize		= sizeof(WNDCLASSEX);
+	wcex.style		= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= (WNDPROC)WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon	= LoadIcon(hInstance, IDI_APPLICATION);
+	wcex.hIconSm= LoadIcon(hInstance, IDI_APPLICATION);
+	wcex.hCursor= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wcex.lpszMenuName	= NULL;
+	wcex.lpszClassName	= szWindowClass;
+
+	if( !RegisterClassEx(&wcex) )
+		return 0;
+
+	HWND hWnd = CreateWindowEx( WS_EX_APPWINDOW
+		, szWindowClass
+		, szTitle
+		, WS_OVERLAPPEDWINDOW
+		, CW_USEDEFAULT
+		, CW_USEDEFAULT
+		, 800
+		, 600
+		, NULL
+		, NULL
+		, hInstance
+		, NULL );
+
+    if (!hWnd)
+		return 0;
+
+	ShowWindow(hWnd, nCmdShow);
+
+	MSG	msg;
+    while (true)
+	{
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+                break;
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else
+        {
+
+        }
+	}
+
+	return (int)msg.wParam;
+}
+
+// 메시지 처리 함수
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	HDC	hdc;
+	PAINTSTRUCT	ps;
+
+	switch(message)
+	{
+	case WM_CREATE:
+
+		break;
+	case WM_PAINT:
+		{
+			hdc = BeginPaint( hWnd, &ps );
+
+			EndPaint( hWnd, &ps );
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
